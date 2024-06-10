57c010c9c36240926c1dd0bd7eb8d61970052072
hujianbo <466636709@qq.com>
2024-06-10 23:43:08 +0800

    feat: add css module

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,5 @@ import { createRoot } from "react-dom/client";
 import React, { ReactElement } from "react";
 import { App } from "@/App";
 
-
-
 const root = createRoot(document.getElementById("app") as HTMLElement);
 root.render(<App />);
